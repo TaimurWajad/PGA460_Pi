b1fc90be94011e7321c30a3f043466bfa3a9d187
TW\Taimur <taimur.wajad786@gmail.com>
2024-06-02 15:38:27 +0200

    PGA450

---
+++ code/PGA450_Pi/main.cpp
@@ -0,0 +1,75 @@
+#include <wiringPi.h>
+#include <wiringSerial.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define UART_DEVICE "/dev/serial0"  // Default UART device on Raspberry Pi 4
+#define BAUD_RATE 9600              // Set baud rate to match PGA450
+
+void sendBytes(int fd, unsigned char *data, int length) {
+    for (int i = 0; i < length; i++) {
+        serialPutchar(fd, data[i]);
+    }
+}
+
+int receiveBytes(int fd, unsigned char *buffer, int maxLength) {
+    int received = 0;
+
+    while (serialDataAvail(fd) > 0 && received < maxLength) {
+        buffer[received++] = serialGetchar(fd);
+        usleep(1000);  // Small delay to allow data to be received
+    }
+
+    return received;
+}
+
+int main() {
+    int fd;
+    unsigned char txData[4] = {0x00, 0x55, 0x01, 0x00}; // Example data to send
+    unsigned char rxData[8];  // Buffer to store received data
+    int receivedLength;
+
+    // Initialize WiringPi and UART
+    if (wiringPiSetup() == -1) {
+        fprintf(stderr, "Failed to initialize WiringPi\n");
+        return 1;
+    }
+
+    if ((fd = serialOpen(UART_DEVICE, BAUD_RATE)) < 0) {
+        fprintf(stderr, "Unable to open serial device: %s\n", strerror(errno));
+        return 1;
+    }
+
+    // Endless loop to send and receive data
+    while (1) {
+        // Send 4 bytes
+        sendBytes(fd, txData, 4);
+
+        // Wait for data to be available (this is an example, you might want to implement a better waiting mechanism)
+        usleep(5000);  // Wait for 5 milliseconds
+
+        // Receive data (up to 256 bytes in this example)
+        receivedLength = receiveBytes(fd, rxData, sizeof(rxData));
+
+        // Print received data in hexadecimal format
+        if (receivedLength > 0) {
+            printf("Received data: ");
+            for (int i = 0; i < receivedLength; i++) {
+                printf("0x%02X ", rxData[i]);
+            }
+            printf("\n");
+        } else {
+            printf("No data received\n");
+        }
+
+        // Sleep for a while before the next iteration
+        usleep(1000000);  // Wait for 1 second before sending data again
+    }
+
+    // Close the UART connection (this part will never be reached in the endless loop)
+    serialClose(fd);
+
+    return 0;
+}
