dd7835c4c3d4d893266b0eae9d23733f23d246d5
TW\Taimur <taimur.wajad786@gmail.com>
2024-12-07 22:39:39 +0500

    Dbg

--- code/PGA460_Pi/PGA460_Pi_Main.cpp
+++ code/PGA460_Pi/PGA460_Pi_Main.cpp
@@ -101,7 +101,7 @@ void initPGA460()
     }
 
     // Enable power to ultrasonic sensors
-    digitalWrite(ULTRASONIC_PWR_EN, HIGH);
+    digitalWrite(ULTRASONIC_PWR_EN, LOW);
     SELECT_SENSOR_1();
 	
 	  initVariables();
