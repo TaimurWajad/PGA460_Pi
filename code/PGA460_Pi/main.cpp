4cbda58fa719c9d583580c627fe2f7e072851958
TW\Taimur <taimur.wajad786@gmail.com>
2024-12-21 14:30:59 +0500

    Dbg

--- code/PGA460_Pi/main.cpp
+++ code/PGA460_Pi/main.cpp
@@ -97,7 +97,7 @@ int main() {
 
     // Enable power to ultrasonic sensors
     digitalWrite(ULTRASONIC_PWR_EN, HIGH);
-    SELECT_SENSOR_2();
+    SELECT_SENSOR_1();
 
     if ((fd = serialOpen(UART_DEVICE, BAUD_RATE)) < 0) 
 	{
