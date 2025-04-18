4590250c8b414492a26353a4407a2adaee3d156c
TW\Taimur <taimur.wajad786@gmail.com>
2025-04-18 09:51:49 +0200

    DBG

--- code/PGA460_Pi/PGA460_Pi_Main.cpp
+++ code/PGA460_Pi/PGA460_Pi_Main.cpp
@@ -323,9 +323,9 @@ int main()
 	initPGA460();
 	while(1)
 	{
-		printf("Sensor 1: \n");
-		SELECT_SENSOR_1();
-		Cyclic_Task();
+		//printf("Sensor 1: \n");
+		//SELECT_SENSOR_1();
+		//Cyclic_Task();
 		//printf("Retrieving echo data dump profile. Wait...\n");
 		//runEchoDataDump(i-1, Serial_Port);                  // run preset 1 or 2 burst and/or listen command
 		//pullEchoDataDumpBulk(Serial_Port);
@@ -336,7 +336,7 @@ int main()
 		//printf("Retrieving echo data dump profile. Wait...\n");
 		//runEchoDataDump(i-1, Serial_Port);                  // run preset 1 or 2 burst and/or listen command
 		//pullEchoDataDumpBulk(Serial_Port);
-		usleep(200000); // (25 milliseconds)
+		usleep(300000); // (25 milliseconds)
 		
 		//if(i<10)
 		//{
