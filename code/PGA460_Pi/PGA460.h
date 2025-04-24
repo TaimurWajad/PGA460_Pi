a7d40d32947dd11e873c64b350b64c78da4c3497
TW\Taimur <taimur.wajad786@gmail.com>
2025-04-24 15:14:36 +0200

    Evm_Dbg

--- code/PGA460_Pi/PGA460.h
+++ code/PGA460_Pi/PGA460.h
@@ -3,7 +3,7 @@
 #include <stdint.h>  // Include this header for uint8_t and uint32_t
 #include <string>
 
-#define MAX_MILLIS_TO_WAIT 250 // Maximum time to wait in milliseconds
+#define MAX_MILLIS_TO_WAIT 200 // Maximum time to wait in milliseconds
 #define UART_DEVICE "/dev/ttyAMA0"  		 // Default UART device on Raspberry Pi 4
 #define BAUD_RATE 115200              	 // // UART baud rate: 9600, 19200, 38400, 57600, 74800, 115200 
 #define UART_RX_PIN 13              	 // GPIO15 corresponds to UART RX
