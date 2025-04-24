2c65befdc4e82415d51c8dcfe9fa513cb62d2fdc
TW\Taimur <taimur.wajad786@gmail.com>
2025-04-24 22:08:35 +0200

    Evm_Dbg

--- code/PGA460_Pi/PGA460_Pi_Main.cpp
+++ code/PGA460_Pi/PGA460_Pi_Main.cpp
@@ -196,7 +196,7 @@ void initPGA460()
     if ((Serial_Port = serialOpen(UART_DEVICE, BAUD_RATE)) < 0) 
 	{
         fprintf(stderr, "Unable to open any serial device: %s\n", strerror(errno));
-		while(1){fprintf(stderr, "Err\n", strerror(errno));};
+		while(1){fprintf(stderr, "Err\n");usleep(1000000000);}
     }
 	
 	usleep(100000);  // Wait for 100 ms before sending data
